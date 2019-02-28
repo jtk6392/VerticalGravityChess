5468162992e929f159f05a54ca2c9ef6cc15489d
nsp5488 <nsp5488@g.rit.edu>
2019-02-27 20:19:40 -0500

    Finished the main game loop and Menu, added a GUI class that i'll eventually implement.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,23 +1,55 @@
 package Game;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 public class Game {
 
+    private static final Scanner sc = new Scanner(System.in);
+
+    /**
+     * Main menu.
+     * @return The choice for the menu.
+     */
     public static char menu() {
-        return 'X';
+        boolean control = true;
+        String line;
+        char c = 'x';
+        System.out.println("MENU");
+        System.out.println("Press P to play");
+        System.out.println("Press Q to quit");
+
+        //while(control) {
+            try{
+                System.out.print("Enter your choice: ");
+                line = Game.sc.nextLine();
+                    if (line.length() == 1) {
+                        c = line.charAt(0);
+                        control = false;
+                    }
+            } catch(NoSuchElementException e) {
+                System.err.println(e.getMessage());
+                c = 'Q';
+            }
+        return c;
     }
 
+    /**
+     * Main game loop.
+     */
     public static void playGame() {
         boolean done = false;
         Board b = new Board();
-        Scanner sc = new Scanner(System.in);
         Board.Player player = Board.Player.PLAYER1;
         int p = 1;
         int play;
         while (!done) {
             System.out.println(b);
             System.out.print("Enter your play: ");
-            play = sc.nextInt();
+
+            play = Game.sc.nextInt();
+            Game.sc.nextLine();
+
             if (b.play(play, player)) {
                 System.out.println(b);
                 done = b.playerWon(play, p);
@@ -39,18 +71,25 @@ public class Game {
         }
     }
 
+    /**
+     * Entry point to the game currently.
+     * @param args
+     */
     public static void main(String[] args) {
         char choice = 'X';
-
-        /*
         while(choice != 'Q'){
             choice = menu();
+            choice = Character.toUpperCase(choice);
             switch(choice) {
-                case 'P':playGame();
+                case 'P': playGame();
+                    break;
                 case 'Q':
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice\n");
+                    break;
             }
         }
-    */
-        playGame();
     }
 }
